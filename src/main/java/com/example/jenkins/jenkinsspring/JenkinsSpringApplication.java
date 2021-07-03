62655627d35831aab87abe927705d5860a6d4046
aayushkarki682 <ayush.karki12345@gmail.com>
2021-07-03 15:58:24 -0500

    first commit

---
+++ src/main/java/com/example/jenkins/jenkinsspring/JenkinsSpringApplication.java
@@ -0,0 +1,25 @@
+package com.example.jenkins.jenkinsspring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinsSpringApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(JenkinsSpringApplication.class);
+
+    @PostConstruct
+    public  void init(){
+        logger.info("Application started ...");
+    }
+    public static void main(String[] args) {
+
+        logger.info("Application executed... ");
+        SpringApplication.run(JenkinsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins/jenkinsspring/JenkinsSpringApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.jenkins.jenkinsspring;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinsSpringApplicationTests {
+
+    public static Logger logger = LoggerFactory.getLogger(JenkinsSpringApplicationTests.class);
+    @Test
+    void contextLoads() {
+        logger.info("Test started ... ");
+        assertEquals(true, true);
+    }
+
+}
